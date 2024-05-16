6f05d2c42d7f686f166cae45149e4687b9eda30c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-16 14:36:05 -0300

    Create Tarefas.vue

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,44 @@
+<template>
+  <FormCount @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <TarefaConcluida v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+    <BoxVazio v-if="listaVazia">
+      Você ainda não realizou nenhuma tarefa.
+    </BoxVazio>
+  </div>
+
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import FormCount from '../components/FormCount.vue';
+import TarefaConcluida from '../components/TarefaConcluida.vue';
+import ITarefa from '../interfaces/ITarefa';
+import BoxVazio from '../components/BoxVazio.vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    FormCount,
+    TarefaConcluida,
+    BoxVazio
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+    }
+  },
+  computed: {
+    listaVazia(): boolean {
+      return this.tarefas.length === 0
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa);
+    }
+  }
+});
+</script>
