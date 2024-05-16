9b00ffa8ca70000aa26c2b4dd5af8b768bf78615
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-16 14:36:15 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <BarraLateral @mudouTema="trocaTema" />
     </div>
     <div class="column is-three-quarter conteudo">
-      <FormCount @aoSalvarTarefa="salvarTarefa" />
-      <div class="lista">
-        <TarefaConcluida v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
-        <BoxVazio v-if="listaVazia">
-          Você ainda não realizou nenhuma tarefa.
-        </BoxVazio>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 
@@ -19,35 +13,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
-import FormCount from './components/FormCount.vue';
-import TarefaConcluida from './components/TarefaConcluida.vue';
-import ITarefa from './interfaces/ITarefa';
-import BoxVazio from './components/BoxVazio.vue';
-
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    FormCount,
-    TarefaConcluida,
-    BoxVazio
   },
   data() {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
-  computed: {
-    listaVazia(): boolean {
-      return this.tarefas.length === 0
-    }
-  },
   methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa);
-    },
     trocaTema(modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo;
     }
