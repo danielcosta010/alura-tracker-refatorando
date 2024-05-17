47255f41f21f9451448a524dcda8043d97ad22c0
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-17 03:59:11 -0300

    Update index.ts

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,10 +1,16 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
-import Tarefas from '../views/Tarefas.vue'
+import Tarefas from '../views/Tarefas.vue';
+import ProjetosTraker from '../views/ProjetosTraker.vue'
 
 const rotas: RouteRecordRaw[] = [{
   path: "/",
   name: 'tarefas',
   component: Tarefas
+},
+{
+  path: '/projetosTraker',
+  name: 'ProjetosTraker',
+  component: ProjetosTraker
 }];
 
 const roteador = createRouter({
