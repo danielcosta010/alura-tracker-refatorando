bbf28b7357f935e82d4e72b10306cb09c46aacd7
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-19 02:13:09 -0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
-import { Store, createStore } from "vuex";
+import { Store, createStore, useStore as vuexUseStore } from "vuex";
 
 interface Estado {
   projetos: IProjeto[]
@@ -11,5 +11,18 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
   state: {
     projetos: []
+  },
+  mutations: {
+    'ADICIONA_PROJETO'(state, nomeDoProjeto) {
+      const projeto = {
+        id: new Date().toISOString(),
+        nome: nomeDoProjeto
+      } as IProjeto
+      state.projetos.push(projeto)
+    }
   }
 })
+
+export default function useStore() : Store<Estado> {
+  return vuexUseStore(key)
+}
