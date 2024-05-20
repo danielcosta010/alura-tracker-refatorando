2437317187165d8a1812a6229d5b0ce4de2ad6f1
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-20 13:49:18 -0300

    Create FormularioTraker.vue

---
+++ src/views/Projetos/FormularioTraker.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="projetos">
+    
+      <h1 class="title">Projetos</h1>
+
+      <form action="" @submit.prevent="salvar">
+        <div class="field">
+          <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
+          <input type="text" id="nomeDoProjeto" v-model="nomeDoProjeto" class="input"
+            placeholder="Digite o nome do projeto">
+        </div>
+        <div class="field">
+          <button class="button" type="submit">Salvar</button>
+        </div>
+      </form>
+  </section>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import  useStore  from '@/store'
+
+export default defineComponent({
+  name: 'FormularioTraker',
+  data() {
+    return {
+      nomeDoProjeto: '',
+      
+    }
+  },
+  methods: {
+    salvar() {
+      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+      this.nomeDoProjeto = ''
+    }
+  },
+  setup() {
+    const store = useStore()
+    return {
+      store
+    }
+  }
+})
+</script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
