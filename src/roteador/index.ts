96eed73cf222c9a6045c3b89f754019297c31d87
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-20 14:44:25 -0300

    Update index.ts

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,17 +1,38 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 import Tarefas from '../views/Tarefas.vue';
-import ProjetosTraker from '../views/ProjetosTraker.vue'
+import ProjetosTraker from '../views/ProjetosTraker.vue';
+import FormularioTraker from '../views/Projetos/FormularioTraker.vue'
 
-const rotas: RouteRecordRaw[] = [{
-  path: "/",
-  name: 'tarefas',
-  component: Tarefas
-},
-{
-  path: '/projetosTraker',
-  name: 'ProjetosTraker',
-  component: ProjetosTraker
-}];
+const rotas: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: 'tarefas',
+    component: Tarefas
+  },
+  {
+    path: "/projetosTraker",
+    component: ProjetosTraker,
+    children: [
+      {
+        path: '',
+        name: 'ProjetosTraker',
+        component: ProjetosTraker
+      },
+      {
+        path: '/novo',
+        name: 'Novo Projto',
+        component: FormularioTraker
+      },
+      {
+        path: '/:id',
+        name: 'Editar Projto',
+        component: FormularioTraker,
+        props: true
+      },
+    ]
+  }
+  
+]
 
 const roteador = createRouter({
   history: createWebHashHistory(),
