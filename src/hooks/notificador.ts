17a25292370cb2a1cb7fe22d7664f5b0c6fa1321
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-25 06:20:12 -0300

    Create notificador.ts

---
+++ src/hooks/notificador.ts
@@ -0,0 +1,22 @@
+import { TipoNotificacao } from "@/interfaces/INotificacao"
+import { store } from "@/store"
+import { NOTIFICAR } from "@/store/tipos-de-mutacoes"
+
+type Notificador = {
+  notificar: (tipo: TipoNotificacao, titulo: string, texto: string) => void
+
+}
+
+export default () : Notificador => {
+  const notificar = (tipo: TipoNotificacao, titulo: string, texto: string) : void => {
+    store.commit(NOTIFICAR, {
+      titulo,
+      texto,
+      tipo
+    })
+  }
+
+  return {
+    notificar
+  }
+}
